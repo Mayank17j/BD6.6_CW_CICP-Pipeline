3c350b5904babaca0180d3aca0c04ac0b72b2c72
Mayank17j <mayank17j@gmail.com>
2025-01-04 13:48:41 +0530

    Initial commit

---
+++ BD6.6_CW/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const { getAllEmployees, getEmployeesById } = require('./controllers');
+
+//Exercise 1: Retrieve All Employees
+app.get('/employees', async (req, res) => {
+  const employees = getAllEmployees();
+  res.json({ employees });
+});
+
+//Exercise 2: Retrieve Employee by ID
+app.get('/employees/details/:id', async (req, res) => {
+  const employee = getEmployeesById(parseInt(req.params.id));
+  res.json({ employee });
+});
+
+module.exports = { app };
